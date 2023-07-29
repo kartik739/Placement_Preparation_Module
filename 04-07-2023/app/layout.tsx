c25687ffd1f408aec0a6ce9e76bcba4d8a5f7b29
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-07-29 22:35:20 +0530

    update
    	deleted:    componets/Board.tsx
    	deleted:    componets/Column.tsx
    	deleted:    componets/header.tsx
    	../03-07-2023/componets/
    	../04-07-2023/app/
    
    	renamed:    componets/Board.tsx -> ../03-07-2023/componets/Board.tsx
    	renamed:    componets/Column.tsx -> ../03-07-2023/componets/Column.tsx
    	renamed:    componets/header.tsx -> ../03-07-2023/componets/header.tsx
    	new file:   ../04-07-2023/app/favicon.ico
    	new file:   ../04-07-2023/app/globals.css
    	new file:   ../04-07-2023/app/layout.tsx
    	new file:   ../04-07-2023/app/page.tsx
    
    	renamed:    componets/Board.tsx -> ../03-07-2023/componets/Board.tsx
    	renamed:    componets/Column.tsx -> ../03-07-2023/componets/Column.tsx
    	renamed:    componets/header.tsx -> ../03-07-2023/componets/header.tsx
    	new file:   ../04-07-2023/app/favicon.ico
    	new file:   ../04-07-2023/app/globals.css
    	new file:   ../04-07-2023/app/layout.tsx
    	new file:   ../04-07-2023/app/page.tsx
    	modified:   ../05-07-2023/Readme.md

---
+++ 04-07-2023/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Trello 2.0',
+  description: 'Created by Kartik Goel',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className='bg-[#F5F6F8]'>{children}</body>
+    </html>
+  )
+}
+
+
+ 
